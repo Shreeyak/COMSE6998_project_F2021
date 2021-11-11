aefb581afd3eecc3c1385e58f9be2e966713211c
Matthew Román <m.roman@columbia.edu>
2021-11-11 18:18:55 -0500

    Add project_code directory and rotation_generator library file with description (no implementation yet)

---
+++ project_code/rotation_generator.py
@@ -0,0 +1,12 @@
+"""
+rotation_generator.py: A library to generate small 3D rotation matrices
+
+The class generates a 3x3 matrix describing a rotation in 3D space, 
+where each element relevant to the rotation is an angle (in radians) 
+whose minimum is 0 (inclusive) and whose maximum is specified by user 
+input. This is intended to create "small" rotations, ___
+
+# TODO: research and suggest how to define a "small" angle
+"""
+
+# Insert class here lol (I'll do it later tonight)
