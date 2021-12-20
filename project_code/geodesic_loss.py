65e73cceba925f90fa6fd2d016912788b9879df4
Shreeyak <shreeyak.sajjan@gmail.com>
2021-12-19 21:05:16 -0500

    added method to create orthonormal rotation matrix from 6d prediction, added optimized geodist loss func

--- project_code/geodesic_loss.py
+++ project_code/geodesic_loss.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from scipy.spatial.transform import Rotation as R
 import scipy.linalg
 
@@ -70,25 +71,107 @@ def geodesic_dist(mata: torch.Tensor, matb: torch.Tensor) -> torch.Tensor:
     return dists / mata.shape[0]  # div by batch size
 
 
+def geodesic_dist_opt(m1: torch.Tensor, m2: torch.Tensor):
+    """Calculates the geodesic distance between 2 3x3 rotation matrices as the difference in angle between them
+
+    Ref:
+        Zhou, Yi, et al. "On the continuity of rotation representations in neural networks."
+        Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition. 2019.
+        Sec 5.1
+    """
+    batch = m1.shape[0]
+    m = torch.bmm(m1, m2.transpose(1, 2))  # batch*3*3
+
+    cos = (m[:, 0, 0] + m[:, 1, 1] + m[:, 2, 2] - 1) / 2
+    cos = torch.min(cos, torch.ones(batch, requires_grad=True, device=m.device))
+    cos = torch.max(cos, torch.ones(batch, requires_grad=True, device=m.device) * -1)
+
+    theta = torch.acos(cos)  # Shape: [B]
+
+    # theta = torch.min(theta, 2*np.pi - theta)
+    return theta
+
+
+class GeodesicDist(nn.Module):
+    """Calculate Geodesic Distance between two batches of 3x3 orthonormal rotation matrices.
+     Wrapper around `geodesic_dist_opt`.
+
+    Inputs:
+        - inputs: Rotation Matrix. Shape: [B, 3, 3]
+        - targets: Rotation Matrix. Shape: [B, 3, 3]
+
+    Outputs:
+        - Tensor: loss
+
+    Args:
+        reduction: How to reduce the loss:
+         - "none": No reduction. Output is of shape: [B].
+         - "sum": sum of all losses
+         - "mean": Mean of all losses
+
+    """
+
+    def __init__(self, reduction: str = "mean"):
+        super().__init__()
+        valid_reductions = ["sum", "mean", "none"]
+        if reduction not in valid_reductions:
+            raise ValueError(f"Invalid reduction: {self.reduction}. Valid value: {valid_reductions}")
+
+        self.reduction = reduction
+
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor):
+        loss_geo = geodesic_dist_opt(inputs, targets)
+
+        if self.reduction == "sum":
+            loss = loss_geo.sum()
+        elif self.reduction == "mean":
+            loss = loss_geo.mean()
+        elif self.reduction == "none":
+            loss = loss_geo
+        else:
+            raise NotImplementedError
+
+        return loss
+
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     # Check the geodesic distance as value increases
 
     r = R.from_euler("x", 0, degrees=True)
-    rot0 = torch.tensor(r.as_matrix()).unsqueeze(0)
+    rot0 = torch.tensor(r.as_matrix()).unsqueeze(0).float()
 
-    dist_list = []
-    for angle in range(0, 360, 5):
-        print(angle)
-        r = R.from_euler("x", angle, degrees=True)
-        rot_mat = r.as_matrix()
-        rot_mat = torch.tensor(rot_mat).unsqueeze(0)  # Shape: (1, 3, 3)
 
-        dist = geodesic_dist(rot0, rot_mat)  # Input Shape: (1, 3, 3)
-        dist_list.append(dist.item())
+    distances = []  # List of distances from each loss func
+    angles = list(range(0, 360, 5))
+    for loss_func in [geodesic_dist, geodesic_dist_opt]:
+        dist_list = []
+        for angle in angles:
+            r = R.from_euler("x", angle, degrees=True)
+            rot_mat = r.as_matrix()
+            rot_mat = torch.tensor(rot_mat).unsqueeze(0).float()  # Shape: (1, 3, 3)
 
-    plt.plot(range(0, 360, 5), dist_list)
+            dist = loss_func(rot0, rot_mat)  # Input Shape: (1, 3, 3)
+            dist_list.append(dist.item())
+        distances.append(dist_list)
+        # plt.plot(range(0, 360, 5), dist_list)
+        # plt.xlabel("Difference in angle")
+        # plt.ylabel("Geodesic Distance")
+        # plt.show()
+
+
+    fig, axs = plt.subplots(2)
+    fig.suptitle('Geodesic Loss between Two 3x3 Rotation Matrices')
+
+    axs[0].plot(angles, distances[0])
+    axs[0].set_title('Using matrix log. \nRef: "3d pose regression using convolutional neural networks."')
     plt.xlabel("Difference in angle")
     plt.ylabel("Geodesic Distance")
+
+    axs[1].plot(angles, distances[1])
+    axs[1].set_title('Using Cosine of Angles. \nRef: "On the continuity of rotation representations in neural networks"')
+    plt.xlabel("Difference in angle")
+    plt.ylabel("Geodesic Distance")
+    plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=3.0)
     plt.show()
--- project_code/train_lightning.py
+++ project_code/train_lightning.py
@@ -15,7 +15,7 @@ from pytorch_lightning.utilities import rank_zero_only
 from rotation_dataset import RotationDataset
 from torch.utils.data import DataLoader
 from pytorch_lightning.plugins import DDPPlugin
-from geodesic_loss import geodesic_dist
+from geodesic_loss import GeodesicDist
 
 
 class Stage(Enum):
@@ -26,6 +26,61 @@ class Stage(Enum):
     TEST = "Test"
 
 
+class OrthoMatrix:
+    """Functions to create an Orthonormal Matrix from the prediction of the model.
+    Borrowed from code of paper "On The Continuity of Rotation Representations in Neural Networks"
+
+    Ref:
+        https://github.com/papagina/RotationContinuity
+
+        Zhou, Yi, et al. "On the continuity of rotation representations in neural networks."
+        Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition. 2019.
+    """
+
+    # batch*n
+    @staticmethod
+    def normalize_vector(v, return_mag=False):
+        batch = v.shape[0]
+        v_mag = torch.sqrt(v.pow(2).sum(1))  # batch
+        eps = torch.tensor([1e-8], dtype=torch.float, device=v.device, requires_grad=True)
+        v_mag = torch.max(v_mag, eps)
+        v_mag = v_mag.view(batch, 1).expand(batch, v.shape[1])
+        v = v / v_mag
+        if (return_mag == True):
+            return v, v_mag[:, 0]
+        else:
+            return v
+
+    # u, v batch*n
+    @staticmethod
+    def cross_product(u, v):
+        batch = u.shape[0]
+        # print (u.shape)
+        # print (v.shape)
+        i = u[:, 1] * v[:, 2] - u[:, 2] * v[:, 1]
+        j = u[:, 2] * v[:, 0] - u[:, 0] * v[:, 2]
+        k = u[:, 0] * v[:, 1] - u[:, 1] * v[:, 0]
+
+        out = torch.cat((i.view(batch, 1), j.view(batch, 1), k.view(batch, 1)), 1)  # batch*3
+
+        return out
+
+    def compute_rotation_matrix_from_ortho6d(self, ortho6d):
+        x_raw = ortho6d[:, 0:3]  # batch*3
+        y_raw = ortho6d[:, 3:6]  # batch*3
+
+        x = self.normalize_vector(x_raw)  # batch*3
+        z = self.cross_product(x, y_raw)  # batch*3
+        z = self.normalize_vector(z)  # batch*3
+        y = self.cross_product(z, x)  # batch*3
+
+        x = x.view(-1, 3, 1)
+        y = y.view(-1, 3, 1)
+        z = z.view(-1, 3, 1)
+        matrix = torch.cat((x, y, z), 2)  # batch*3*3
+        return matrix
+
+
 class RotationNet(nn.Module):
     def __init__(self):
         super().__init__()
@@ -33,15 +88,15 @@ class RotationNet(nn.Module):
         self.layer2 = nn.Linear(4*32*32, 2*16*16)
         self.layer3 = nn.Linear(2 * 16 * 16, 1*8*8)
         self.layer4 = nn.Linear(1 * 8 * 8, 4*4)
-        self.layer5 = nn.Linear(4*4, 9)
+        self.layer5 = nn.Linear(4*4, 6)
         self.model = nn.Sequential(self.layer1, self.layer2, self.layer3, self.layer4, self.layer5)
 
+        self.ortho_mat = OrthoMatrix()
+
     def forward(self, x):
-        x = self.model(x)
-        bsize = x.shape[0]
-        x = x.reshape((bsize, 3, 3))
-        q, _ = torch.linalg.qr(x)
-        return q
+        x = self.model(x)  # Shape: [B, 6]
+        rot_mat = self.ortho_mat.compute_rotation_matrix_from_ortho6d(x)  # Shape: [B, 3, 3]
+        return rot_mat
 
 
 class RotationNetPl(pl.LightningModule):
@@ -50,6 +105,8 @@ class RotationNetPl(pl.LightningModule):
         # self.save_hyperparameters()  # Will save args to hparams attr. Also allows upload of config to wandb.
 
         self.model = RotationNet()
+
+        self.geo_dist = GeodesicDist(reduction="mean")
 
     def forward(self, inputs: torch.Tensor):
         """
@@ -69,8 +126,8 @@ class RotationNetPl(pl.LightningModule):
         # preds = self(inputs)
         preds = self.model(inputs)
 
-        loss = geodesic_dist(preds, labels)
-        self.log(f"{stage.value}/loss", loss,on_step=False, on_epoch=True)
+        loss = self.geo_dist(preds, labels)
+        self.log(f"{stage.value}/loss", loss, on_step=False, on_epoch=True)
 
         outputs = {
             "loss": loss
@@ -115,7 +172,7 @@ def main():
         raise ValueError(f"Dir does not exist: {train_dir}")
 
     train_dataset = RotationDataset(str(train_dir) + '/', True)
-    train_loader = DataLoader(train_dataset, batch_size=5, shuffle=True, num_workers=0, drop_last=True)
+    train_loader = DataLoader(train_dataset, batch_size=5, shuffle=True, num_workers=8, drop_last=True)
 
     trainer = pl.Trainer(
         logger=wb_logger,
